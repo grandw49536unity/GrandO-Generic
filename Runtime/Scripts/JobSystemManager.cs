4756f07a4d205615037c4c288b28b989d10d3402
kclee <grando93290@gmail.com>
2025-07-07 17:29:15 +0800

    Update jobs system manager

--- Runtime/Scripts/JobSystemManager.cs
+++ Runtime/Scripts/JobSystemManager.cs
@@ -22,20 +22,33 @@ namespace GrandO.Generic {
 		}
 		
 		private List<JobHandle> jobHandles;
-
+		private List<JobHandle> lateJobHandles;
+		
 		public void ScheduleJobComplete(JobHandle _jobHandle) { 
 			jobHandles.Add(_jobHandle);
 		}
 
+		public void ScheduleLateJobComplete(JobHandle _jobHandle) { 
+			lateJobHandles.Add(_jobHandle);
+		}
+
 		private void Initialize() {
 			jobHandles = new List<JobHandle>();
+			lateJobHandles = new List<JobHandle>();
 		}
-		
-		private void LateUpdate() {
+
+		private void Update() { 
 			for (int i = 0; i < jobHandles.Count; ++i) {
 				jobHandles[i].Complete();
 			}
 			jobHandles.Clear();
+		}
+
+		private void LateUpdate() {
+			for (int i = 0; i < lateJobHandles.Count; ++i) {
+				lateJobHandles[i].Complete();
+			}
+			lateJobHandles.Clear();
 		}
 
 	}
