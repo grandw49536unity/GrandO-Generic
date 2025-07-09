fe7bdb4f5bf9c73ce1ef2b24cb9c31127732d623
kclee <grando93290@gmail.com>
2025-07-09 13:17:02 +0800

    update transform job batching

---
+++ Runtime/Scripts/JobSystemManager.TransformJob.cs
@@ -0,0 +1,98 @@
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.Jobs;
+
+using Unity.Mathematics;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+
+namespace GrandO.Generic {
+
+	public partial class JobSystemManager {
+
+		public enum TransformUpdateRule {
+			NoUpdate = 0,
+			Local,
+			WorldSpace,
+			LocalPositionOnly,
+			LocalRotationOnly,
+			WorldSpacePositionOnly,
+			WorldSpaceRotationOnly
+		}
+
+		[BurstCompile]
+		public struct TransformJobData {
+			public TransformUpdateRule rule;
+			public float3 position;
+			public quaternion rotation;
+		}
+
+		public bool ValidateTransformJob() {
+			int transformCount = transformJobDataList.Count;
+			if (transformCount == 0) return false;
+			if (!transformJobDataArray.IsCreated) {
+				transformJobDataArray = new NativeArray<TransformJobData>(transformJobDataList.ToArray(), Allocator.Persistent);
+				transformAccessArray = new TransformAccessArray(transformList.ToArray(), 8);
+				isTransformListUpdated = false;
+			}
+			if (isTransformListUpdated) {
+				if (transformJobDataArray.Length == transformCount) {
+					for (int i = 0; i < transformCount; i++) transformJobDataArray[i] = transformJobDataList[i];
+				} else {
+					transformJobDataArray.Dispose();
+					transformJobDataArray = new NativeArray<TransformJobData>(transformJobDataList.ToArray(), Allocator.Persistent);
+				}
+				transformAccessArray.SetTransforms(transformList.ToArray());
+			}
+			return true;
+		}
+
+		// int GetDesiredJobCount(int transformCount) {
+		// 	if (transformCount <= 128) return 1;
+		// 	if (transformCount <= 512) return 2;
+		// 	if (transformCount <= 2048) return 4;
+		// 	if (transformCount <= 4096) return 8;
+		// 	if (transformCount <= 8192) return 16;
+		// 	return 24; // cap around 10k+
+		// }
+
+		public void DestroyTransformJobData() {
+			if (transformJobDataArray.IsCreated) transformJobDataArray.Dispose();
+			if (transformAccessArray.isCreated) transformAccessArray.Dispose();
+		}
+
+		[BurstCompile]
+		public struct TransformJob : IJobParallelForTransform {
+			[ReadOnly] public NativeArray<TransformJobData> transformJobDataArray;
+			public void Execute(int index, TransformAccess transformAccess) {
+				TransformJobData data = transformJobDataArray[index];
+				if (data.rule != TransformUpdateRule.NoUpdate) {
+					switch (data.rule) {
+						case TransformUpdateRule.Local:
+							transformAccess.SetLocalPositionAndRotation(data.position, data.rotation);
+							break;
+						case TransformUpdateRule.WorldSpace:
+							transformAccess.SetPositionAndRotation(data.position, data.rotation);
+							break;
+						case TransformUpdateRule.LocalPositionOnly:
+							transformAccess.localPosition = data.position;
+							break;
+						case TransformUpdateRule.LocalRotationOnly:
+							transformAccess.localRotation = data.rotation;
+							break;
+						case TransformUpdateRule.WorldSpacePositionOnly:
+							transformAccess.position = data.position;
+							break;
+						case TransformUpdateRule.WorldSpaceRotationOnly:
+							transformAccess.rotation = data.rotation;
+							break;
+					}
+				}
+			}
+		}
+
+	}
+
+}
--- Runtime/Scripts/JobSystemManager.cs
+++ Runtime/Scripts/JobSystemManager.cs
@@ -1,25 +1,28 @@
 using System.Collections.Generic;
 
 using UnityEngine;
+using UnityEngine.Jobs;
 
+using Unity.Collections;
 using Unity.Jobs;
 
 namespace GrandO.Generic {
 
 	[DefaultExecutionOrder(1000)]
-	public class JobSystemManager : MonoBehaviour {
+	public partial class JobSystemManager : MonoBehaviour {
 		
 		private static JobSystemManager singleton;
 		public static JobSystemManager GetSingleton() { 
 			return singleton;
 		}
-		public static JobSystemManager CreateSingleton() {
+		public static JobSystemManager CreateSingleton(int _jobCapacity = 256) {
 			if (!singleton) {
-				singleton = GeneralUtilities.CreateGameObjectWithComponent<JobSystemManager>("JobSystemManager");
-				singleton.Initialize();
+				GeneralUtilities.CreateGameObjectWithComponent<JobSystemManager>("JobSystemManager").Initialize(_jobCapacity);
 			}
 			return singleton;
 		}
+
+		private int jobCapacity = 256;
 		
 		private List<JobHandle> jobHandles;
 		private List<JobHandle> afterJobHandles;
@@ -28,6 +31,12 @@ namespace GrandO.Generic {
 		private System.Action onJobComplete;
 		private System.Action onAfterJobComplete;
 		private System.Action onLateJobComplete;
+
+		private bool isTransformListUpdated;
+		private List<Transform> transformList;
+		private List<TransformJobData> transformJobDataList;
+		private NativeArray<TransformJobData> transformJobDataArray;
+		private TransformAccessArray transformAccessArray;
 		
 		public void ScheduleJobComplete(JobHandle _jobHandle) { 
 			jobHandles.Add(_jobHandle);
@@ -53,34 +62,43 @@ namespace GrandO.Generic {
 			lateJobHandles.Add(_jobHandle);
 		}
 
-		private void Initialize() {
-			jobHandles = new List<JobHandle>();
-			afterJobHandles = new List<JobHandle>();
-			lateJobHandles = new List<JobHandle>();
+		private void Initialize(int _jobCapacity = 256) {
+			singleton = this;
+			jobCapacity = _jobCapacity;
+			jobHandles = new List<JobHandle>(jobCapacity);
+			afterJobHandles = new List<JobHandle>(jobCapacity);
+			lateJobHandles = new List<JobHandle>(jobCapacity);
+			transformList = new List<Transform>(jobCapacity*2);
+			transformJobDataList = new List<TransformJobData>(jobCapacity*2);
 		}
 
 		private void Update() { 
-			for (int i = 0; i < jobHandles.Count; ++i) {
-				jobHandles[i].Complete();
-			}
+			
+			for (int i = 0; i < jobHandles.Count; i++) jobHandles[i].Complete();
 			jobHandles.Clear();
 			onJobComplete?.Invoke();
 			onJobComplete = null;
-			for (int i = 0; i < afterJobHandles.Count; ++i) {
-				afterJobHandles[i].Complete();
-			}
+			
+			for (int i = 0; i < afterJobHandles.Count; i++) afterJobHandles[i].Complete();
 			afterJobHandles.Clear();
 			onAfterJobComplete?.Invoke();
 			onAfterJobComplete = null;
+			
 		}
 
 		private void LateUpdate() {
-			for (int i = 0; i < lateJobHandles.Count; ++i) {
-				lateJobHandles[i].Complete();
+
+			if (ValidateTransformJob()) {
+				TransformJob transformJob = new TransformJob() { transformJobDataArray = transformJobDataArray};
+				JobHandle transformJobHandle = transformJob.Schedule(transformAccessArray);
+				ScheduleLateJobComplete(transformJobHandle);
 			}
+
+			for (int i = 0; i < lateJobHandles.Count; i++) lateJobHandles[i].Complete();
 			lateJobHandles.Clear();
 			onLateJobComplete?.Invoke();
 			onLateJobComplete = null;
+			
 		}
 
 	}
