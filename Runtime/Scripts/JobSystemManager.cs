8815db7e275f7e97ad6686f6026e35ec1d498100
kclee <grando93290@gmail.com>
2025-08-07 18:29:23 +0800

    remove transform job first

--- Runtime/Scripts/JobSystemManager.cs
+++ Runtime/Scripts/JobSystemManager.cs
@@ -15,14 +15,17 @@ namespace GrandO.Generic {
 		public static JobSystemManager GetSingleton() { 
 			return singleton;
 		}
-		public static JobSystemManager CreateSingleton(int _jobCapacity = 256) {
+		public static JobSystemManager CreateSingleton(int _jobCapacity = 16) {
 			if (!singleton) {
 				GeneralUtilities.CreateGameObjectWithComponent<JobSystemManager>("JobSystemManager").Initialize(_jobCapacity);
 			}
 			return singleton;
 		}
 
-		private int jobCapacity = 256;
+		private int frameCounter = 0;
+		public int GetFrameCounter() => frameCounter;
+
+		private int jobCapacity = 16;
 		
 		private List<JobHandle> jobHandles;
 		private List<JobHandle> afterJobHandles;
@@ -32,11 +35,11 @@ namespace GrandO.Generic {
 		private System.Action onAfterJobComplete;
 		private System.Action onLateJobComplete;
 
-		private bool isTransformListUpdated;
-		private List<Transform> transformList;
-		private List<TransformJobData> transformJobDataList;
-		private NativeArray<TransformJobData> transformJobDataArray;
-		private TransformAccessArray transformAccessArray;
+		// private bool isTransformListUpdated;
+		// private List<Transform> transformList;
+		// private List<TransformJobData> transformJobDataList;
+		// private NativeArray<TransformJobData> transformJobDataArray;
+		// private TransformAccessArray transformAccessArray;
 		
 		public void ScheduleJobComplete(JobHandle _jobHandle) { 
 			jobHandles.Add(_jobHandle);
@@ -62,14 +65,14 @@ namespace GrandO.Generic {
 			lateJobHandles.Add(_jobHandle);
 		}
 
-		private void Initialize(int _jobCapacity = 256) {
+		private void Initialize(int _jobCapacity = 16) {
 			singleton = this;
 			jobCapacity = _jobCapacity;
 			jobHandles = new List<JobHandle>(jobCapacity);
 			afterJobHandles = new List<JobHandle>(jobCapacity);
 			lateJobHandles = new List<JobHandle>(jobCapacity);
-			transformList = new List<Transform>(jobCapacity*2);
-			transformJobDataList = new List<TransformJobData>(jobCapacity*2);
+			// transformList = new List<Transform>(jobCapacity*2);
+			// transformJobDataList = new List<TransformJobData>(jobCapacity*2);
 		}
 
 		private void Update() { 
@@ -88,17 +91,18 @@ namespace GrandO.Generic {
 
 		private void LateUpdate() {
 
-			if (ValidateTransformJob()) {
-				TransformJob transformJob = new TransformJob() { transformJobDataArray = transformJobDataArray};
-				JobHandle transformJobHandle = transformJob.Schedule(transformAccessArray);
-				ScheduleLateJobComplete(transformJobHandle);
-			}
+			// if (ValidateTransformJob()) {
+			// 	TransformJob transformJob = new TransformJob() { transformJobDataArray = transformJobDataArray};
+			// 	JobHandle transformJobHandle = transformJob.Schedule(transformAccessArray);
+			// 	ScheduleLateJobComplete(transformJobHandle);
+			// }
 
 			for (int i = 0; i < lateJobHandles.Count; i++) lateJobHandles[i].Complete();
 			lateJobHandles.Clear();
 			onLateJobComplete?.Invoke();
 			onLateJobComplete = null;
-			
+
+			frameCounter++;
 		}
 
 	}
